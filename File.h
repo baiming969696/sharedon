7fbb88754b019fc37a042721b831dab12c1145ac
baiming969696 <baiming969696@sina.com>
2014-09-07 23:29:11 +0800

    Update File.h

--- File.h
+++ File.h
@@ -12,3 +12,51 @@ private:
   OwnerCollection<Owner>  ownerCol;
 };
 
+#define SUCCEED  0
+#define ERROR   -1
+
+class CountTrie
+{
+public:
+  CountTrie();
+  ~CountTrie();
+  
+  int findEntry         (const std::string& s);
+  int findClosestEntries(const std::string& s, int num, std::vector<string> result);
+  int insertEntry       (const std::string& s);
+  int deleteEntry       (const std::string& s);
+  
+private:
+  CountTrieNode *head;
+};
+
+int CountTrie::findEntry(const std::string& s)
+{
+  int len = s.size();  
+  if (len == 0) {
+    return ERROR;
+  }
+  CountTrieNode *tmp = head;
+  for (int i = 0; i < len; i++) {
+    tmp = tmp->children[s[i]];
+    if (tmp == NULL) {
+      return ERROR;
+    }
+    if (tmp->isLeaf == true) {
+      if (i == len-1) {
+        return SUCCEED;
+      } else {
+        continue;
+      }
+    }
+  }
+}
+
+int CountTrie::insertEntry(const std::string& s)
+{
+  int len = s.size();
+  if (len == 0) {
+    return ERROR;
+  }
+  Count
+}
